efb40901e46463b6e2273321502ad5e165e636e1
Pranay <pranay.miyan@proton.me>
2025-10-06 20:50:44 +0000

    Testing to make a new calcultor

--- calculator.py
+++ calculator.py
@@ -1,1 +1,25 @@
-print("test")
+
+
+while True:
+    try:
+        num = int(input("What is your digits! : "))
+        num2 = int(input("What is your second digit! : "))
+    except ValueError:
+        print("Please enter correctly!")
+        
+        symbol = input("What operation do you want? - (*, / , + , -) : ")
+        if symbol == "*":
+            print(f"Your answer is {num*num2}.")
+            break
+        elif symbol == "+":
+            print(f"Your answer is {num+num2}.")
+            break
+        elif symbol == "-":
+            print(f"Your answer is {num-num2}.")
+            break
+        elif symbol == "/":
+            print(f"Your answer is {num/num2}.")
+            break
+        else:
+            print("Please choose from:(/, +, -, *)")
+
