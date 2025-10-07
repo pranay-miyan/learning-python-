0a82065df6c40f19ba024deb111b860bdea440b0
Pranay <pranay.miyan@proton.me>
2025-10-07 08:12:35 +0000

    cal 2 and testor

---
+++ calculator2.py
@@ -0,0 +1,10 @@
+
+
+cal = input("What is your digits ? (Enter as many, when finalised all digits press q) : ")
+digits = []
+while cal != "q" or "Q": 
+    cal()
+
+while True:
+    
+    for cal
---
+++ testor.py
@@ -0,0 +1,12 @@
+
+tempmem = []
+while True:
+    cal = int(input("What is your  digits? : "))
+    symbol = input("What is your symbol (choose from: *, /, +, -) : ")
+    tempmem.append(cal)
+    print(tempmem) # for debugging
+    
+    for cal in symbol:
+
+
+ 
