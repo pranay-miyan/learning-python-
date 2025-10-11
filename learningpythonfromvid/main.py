a933ed21ade2d803829ff55c1e4622dd45924a36
Pranay <pranay.miyan@proton.me>
2025-10-11 16:30:56 +0000

    main.py for tutorial lol!

--- learningpythonfromvid/main.py
+++ learningpythonfromvid/main.py
@@ -1,2 +1,12 @@
+# first python program
+"""
 print("I love pizza!")
 print("Its really good")
+
+"""
+
+# strings
+first_name = "Pranay"
+food = "Pizza"
+print(f"This is {first_name}")
+print(f"I love {food}")
