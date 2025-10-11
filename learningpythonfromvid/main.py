50dd9a5098b79303bbf90e96664a8dca122d803a
Pranay <pranay.miyan@proton.me>
2025-10-11 16:42:35 +0000

    all variables

--- learningpythonfromvid/main.py
+++ learningpythonfromvid/main.py
@@ -3,10 +3,46 @@
 print("I love pizza!")
 print("Its really good")
 
-"""
+
 
 # strings
 first_name = "Pranay"
 food = "Pizza"
 print(f"This is {first_name}")
 print(f"I love {food}")
+
+# integer
+age = 25
+quantity = 2
+
+print(f"Your are {age} years old.")
+
+#float
+price = 0.67
+
+
+
+
+
+
+#Boolean
+
+student = input("Are you a student yes or not? : type y or n : ")
+
+if student == "y":
+    student = True
+    print("cool")
+else:
+    student = False
+    print("wow")
+
+if student == True:
+    input("what is your age: ")
+else:
+    print("your not a student sorry.")
+
+"""
+
+
+
+
