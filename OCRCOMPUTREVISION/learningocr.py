0be269792e65a360872cdd29c597504f74ae3da2
Pranay <pranay.miyan@proton.me>
2025-10-25 21:24:06 +0000

    Ocr stuff

---
+++ OCRCOMPUTREVISION/learningocr.py
@@ -0,0 +1,22 @@
+count1 = 0
+count2 = 0
+count3 = 0
+vote = 0
+import random
+
+"""
+while True:
+    vote = input("What is your choice from A, B, C!")
+    if vote == "A":
+        count1 = count1 + 1
+    elif vote == "B":
+        count2 = count2 + 1
+    elif vote == "C":
+        count3 = count3 + 1
+    elif vote == "END":
+        print(count1, count2, count3)
+        print(f"Total = {count1+count2+count3}")
+        break
+"""
+print(random.randint(1,10))
+    
